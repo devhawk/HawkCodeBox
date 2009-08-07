36d037c2a1d80b98372902104315f128d54d4852
Harry Pierson <harry.pierson@microsoft.com>
2009-08-07 11:11:41 -0700

    added python code to sample app

--- HawkCodeBox.Sample/Window1.xaml.cs
+++ HawkCodeBox.Sample/Window1.xaml.cs
@@ -22,6 +22,26 @@ namespace HawkCodeBox.Sample
         public Window1()
         {
             InitializeComponent();
+            codebox.Text = @"import clr
+clr.AddReference(""pygments"")
+
+from pygments import highlight
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.styles import get_all_styles, get_style_by_name
+
+from devhawk_formatter import DevHawkHtmlFormatter
+
+def get_lexers(): 
+  return get_all_lexers()
+
+def get_styles(): 
+  return get_all_styles()
+
+def generate_html(code, lexer_name, style_name):
+  if not lexer_name: lexer_name = ""text""
+  if not style_name: style_name = ""default""
+  lexer = get_lexer_by_name(lexer_name)
+  return highlight(code, lexer, DevHawkHtmlFormatter(style=style_name))";
         }
     }
 }
